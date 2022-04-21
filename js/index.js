13bdb8ae68904129c052f76cf44ac2e4111ea4e0
David Song <learntobestill@gmail.com>
2022-04-21 14:33:25 -0700

    Finalized project. Etch a Sketch with two button variation.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,64 @@
 const mainContainer = document.querySelector('#mainContainer');
-// const blocks = document.createElement('div');
-// blocks.classList.add('blocks');
-// blocks.textContent = "BLOCK";
-
-// mainContainer.appendChild(blocks);
-
 const blockContainer = document.createElement('blockContainer');
 blockContainer.classList.add('blockContainer');
 mainContainer.appendChild(blockContainer);
 
-for (let i = 0; i < 256; i++) {
-    const blocks = document.createElement('div');
-    blocks.classList.add('blocks');
-    blocks.textContent = "";
+const clearIt = document.querySelector('.clearIt');
+const resetIt = document.querySelector('.resetIt');
 
-    blockContainer.appendChild(blocks);
+buildGrid();
+
+clearIt.addEventListener('click', () => {
+    var chooseGrid = Number(prompt("Please Enter grid size (max 100):"));
+    resetNewBoard();
+    buildGrid(chooseGrid);
+    console.log(typeof(chooseGrid));
+});
+
+resetIt.addEventListener('click', () => {
+    resetNewBoard();
+    buildGrid();
+});
+
+
+function resetNewBoard() {
+    const elem = document.getElementsByClassName('blockRow');
+    while(elem.length > 0){
+        elem[0].parentNode.removeChild(elem[0]);
+    }
+    const elem2 = document.getElementsByClassName('blocks');
+    while(elem2.length > 0){
+        elem2[0].parentNode.removeChild(elem2[0]);
+    }
 }
+
+function buildGrid(gridSize = 10) {
+
+    for (let i = 0; i < gridSize; i++) {
+        const blockRow = document.createElement('div');
+        blockRow.classList.add('blockRow');
+        blockContainer.appendChild(blockRow);
+        console.log(blockRow);
+    
+        for (let j = 0; j < gridSize; j++) {
+            const blocks = document.createElement('div');
+            blocks.classList.add('blocks');
+            // blocks.textContent = 'dude';
+            blockRow.appendChild(blocks);
+            // console.log(blocks);
+        };
+    };
+
+    const blocks = document.querySelectorAll('.blocks');
+
+    blocks.forEach(block => {
+        
+        block.addEventListener('mouseenter', () => {
+            block.classList.add('blockColor');
+            console.log(block);
+        });
+    });
+    
+};
+
+
